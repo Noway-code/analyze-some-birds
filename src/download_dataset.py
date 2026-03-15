5115ecf10b76c59d73d59f4bcc6de502bdbf5e56
Noway-code <dragonfury1928@gmail.com>
2026-03-15 16:20:04 -0400

    fix files

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -4,3 +4,16 @@ import requests
 load_dotenv() 
 
 key = os.getenv('EBIRD_API_KEY')
+
+
+url = "https://api.ebird.org/v2/product/top100/US/2025/01/01"
+
+payload={}
+headers = {
+        'x-ebirdapitoken':key
+        }
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
