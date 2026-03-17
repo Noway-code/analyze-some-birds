0e9540dafe96b7b89e0cdc3334b4fa5e11d864d2
Noway-code <dragonfury1928@gmail.com>
2026-03-17 18:40:17 -0400

    yay extracting and classifying each frame

--- src/classifier.py
+++ src/classifier.py
@@ -5,6 +5,40 @@ import ffmpeg
 import datetime
 import os
 import math
+import subprocess
+import numpy as np
+import time
+
+width = 1280
+height = 720
+def subprocess_frames(video_path):
+    process = subprocess.Popen(
+    [
+        "ffmpeg",
+        "-i", video_path,
+        "-f", "image2pipe",
+        "-pix_fmt", "rgb24",
+        "-vcodec", "rawvideo",
+        "-"
+    ],
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE
+    )
+    
+    frame_size = width * height * 3
+    i = 0
+    while True:
+        raw = process.stdout.read(frame_size)
+        if len(raw) != frame_size:
+            break
+
+        frame = np.frombuffer(raw, np.uint8).reshape((height, width, 3))
+        pil_image = Image.fromarray(frame)
+        result = classifier(pil_image)
+        
+        print(result[0]['score'])
+        output_path = os.path.join("validate", f"{time.time()}_{i}.jpg")
+        pil_image.save(output_path)
 
 def sample_frames(idx,video_path, output_folder="out"):
     """
@@ -19,12 +53,7 @@ def sample_frames(idx,video_path, output_folder="out"):
     for i in range(duration_seconds):
         output_path = os.path.join(output_folder, f"vid_{idx}_frame_at_{i}s_{i}.jpg")
         try:
-            (
-                ffmpeg
-                .input(video_path, ss=i) # Seek to the timestamp (ss)
-                .output(output_path, vframes=1) # Output only one frame (vframes=1)
-                .run(overwrite_output=True, quiet=True)
-            )
+            ffmpeg.input(video_path, ss=i).output(output_path, vframes=1).run(overwrite_output=True, quiet=True)
             print(f"Saved frame at {i}s to {output_path}")
         except Exception as e:
             print("welp", e)
@@ -36,10 +65,8 @@ classifier = pipeline(
 directory_path = 'data/*.mp4'
 idx = 0
 for vid in glob.glob(directory_path):
-    sample_frames(idx,vid)
-    idx+=1
- #   with Image.open(image) as img:
- #       print(classifier(img))
+    subprocess_frames(vid)
+    
 
 
 
