913a887c0ca9c2462ea0d8d844607a3693f178af
Noway-code <dragonfury1928@gmail.com>
2026-03-21 08:41:10 -0400

    start fastapi server

--- src/classifier.py
+++ src/classifier.py
@@ -14,9 +14,8 @@ from ultralytics import YOLO
 import pprint
 directory_path = 'data/*.mp4'
 output_path = 'validate'
-model = YOLO("yolov8s.pt")  # use yolov8s.pt if you want better accuracy
+model = YOLO("yolov8n.pt") 
 
-#classifier = pipeline("image-classification", model="dennisjooo/Birds-Classifier-EfficientNetB2")
 
 def get_resolution(cap):
     w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -24,44 +23,6 @@ def get_resolution(cap):
     fps = cap.get(cv2.CAP_PROP_FPS)
     return w, h, fps
 
-#def subprocess_frames(video_path):
-#    process = subprocess.Popen(
-#    [
-#        "ffmpeg",
-#        "-i", video_path,
-#        "-f", "image2pipe",
-#        "-pix_fmt", "rgb24",
-#        "-vcodec", "rawvideo",
-#        "-"
-#    ],
-#    stdout=subprocess.PIPE,
-#    stderr=subprocess.PIPE
-#    )
-#    width, height, _= get_resolution(video_path) 
-#    frame_size = width * height * 3
-#    i = 0
-#    success=0
-#    total=0
-#    print(f"running {video_path}")
-#    while True:
-#        raw = process.stdout.read(frame_size)
-#        if len(raw) != frame_size:
-#            break
-#
-#        frame = np.frombuffer(raw, np.uint8).reshape((height, width, 3))
-#        pil_image = Image.fromarray(frame)
-#        result = classifier(pil_image)
-#        
-#        candidate = result[0]
-#        label = candidate['label']
-#        score = candidate['score']
-#        
-#        total+=1
-#
-#    print(f"{success}, {total}, accuracy: {success/total}")
-#    process.stdout.close()
-#    process.stderr.close()
-#    process.wait()
 def yolo(video_path):
     cap = cv2.VideoCapture(video_path)
     out_path = os.path.join(output_path, os.path.basename(video_path))
