60edf92b11066c20c29ed5bee39cb99bc0036660
Noway-code <dragonfury1928@gmail.com>
2026-03-21 09:26:38 -0400

    send vid to api

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, File, UploadFile
 app = FastAPI()
 
 
@@ -11,3 +10,9 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+# Work past memory limit for images, vids, etc.
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    print(file.filename, file.content_type, file.file)
+    return {"filename": file.filename}
